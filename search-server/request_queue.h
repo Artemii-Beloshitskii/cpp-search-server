6adad0c4cbb11fd68ee88ca4e0e9edc15a2a6ba4
Artemii-Beloshitskii <SckorlupAa@yandex.ru>
2023-05-02 01:31:08 +0300

    5 sprint 2 attempt

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -7,18 +7,7 @@ public:
     explicit RequestQueue(const SearchServer& search_server);
 
     template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-        const std::vector<Document> request_ = search_server_request.FindTopDocuments(raw_query, document_predicate);
-        if (request_.empty()) {
-            no_result++;
-        }
-        if (requests_.size() >= min_in_day_) {
-            requests_.pop_front();
-            no_result--;
-        }
-        requests_.push_front(static_cast<int>(request_.size()));
-        return request_;
-    }
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
 
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
 
@@ -31,3 +20,17 @@ private:
     const SearchServer& search_server_request;
     int no_result;
 };
+
+template <typename DocumentPredicate>
+std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+    const std::vector<Document> request_ = search_server_request.FindTopDocuments(raw_query, document_predicate);
+    if (request_.empty()) {
+        no_result++;
+    }
+    if (requests_.size() >= min_in_day_) {
+        requests_.pop_front();
+        no_result--;
+    }
+    requests_.push_front(static_cast<int>(request_.size()));
+    return request_;
+}
