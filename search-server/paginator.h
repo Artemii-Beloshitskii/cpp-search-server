4d839089c2cbc234bfe9662d3dadf12445aa4d06
Artemii-Beloshitskii <SckorlupAa@yandex.ru>
2023-05-01 23:48:32 +0300

    5 sprint 1 attempt

---
+++ search-server/paginator.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <vector>
+
+template <typename Iterator>
+class IteratorRange {
+public:
+    IteratorRange(Iterator begin, Iterator end) :
+        page_begin(begin),
+        page_end(end)
+    {
+    }
+    const Iterator begin() const {
+        return page_begin;
+    }
+    const Iterator end() const {
+        return page_end;
+    }
+private:
+    Iterator page_begin, page_end;
+};
+
+template <typename Iterator>
+std::ostream& operator<< (std::ostream& out, const IteratorRange<Iterator>& range) {
+    for (Iterator iterator = range.begin(); iterator < range.end(); ++iterator) {
+        out << *iterator;
+    }
+    return out;
+}
+
+template <typename Iterator>
+class Paginator {
+public:
+    Paginator(Iterator begin, Iterator end, size_t page_size) {
+        for (size_t current_begin = std::distance(begin, end); current_begin > 0;) {
+            if (current_begin < page_size) {
+                all_pages.push_back({ begin, std::next(begin, current_begin) });
+                begin = std::next(begin, current_begin);
+                current_begin -= current_begin;
+            }
+            else {
+                all_pages.push_back({ begin, std::next(begin, page_size) });
+                begin = std::next(begin, page_size);
+                current_begin -= page_size;
+            }
+        }
+    }
+    typename std::vector<IteratorRange<Iterator>>::const_iterator size() const {
+        return all_pages.size();
+    }
+    typename std::vector<IteratorRange<Iterator>>::const_iterator begin() const {
+        return all_pages.begin();
+    }
+    typename std::vector<IteratorRange<Iterator>>::const_iterator end() const {
+        return all_pages.end();
+    }
+private:
+    std::vector<IteratorRange<Iterator>> all_pages;
+};
+
+template <typename Container>
+auto Paginate(const Container& c, size_t page_size) {
+    return Paginator(begin(c), end(c), page_size);
+}
---
+++ search-server/request_queue.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <queue>
+#include "search_server.h"
+
+class RequestQueue {
+public:
+    explicit RequestQueue(const SearchServer& search_server);
+
+    template <typename DocumentPredicate>
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+        const std::vector<Document> request_ = search_server_request.FindTopDocuments(raw_query, document_predicate);
+        if (request_.empty()) {
+            no_result++;
+        }
+        if (requests_.size() >= min_in_day_) {
+            requests_.pop_front();
+            no_result--;
+        }
+        requests_.push_front(static_cast<int>(request_.size()));
+        return request_;
+    }
+
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
+
+    std::vector<Document> AddFindRequest(const std::string& raw_query);
+
+    int GetNoResultRequests() const;
+private:
+    std::deque<int> requests_;
+    const static int min_in_day_ = 1440;
+    const SearchServer& search_server_request;
+    int no_result;
+};
